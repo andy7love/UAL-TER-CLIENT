a2cb704f49f476c4ff25d1be4a9927a09c6a22b5
Andres Amor <andy.7love@gmail.com>
2017-02-07 00:13:06 -0300

    First version of pitch/roll display on HUD.

--- src/modules/HUD.ts
+++ src/modules/HUD.ts
@@ -16,5 +16,18 @@ export class HUD {
 		let hud = document.createElement('div');
 		hud.innerHTML = html;
 		document.body.appendChild(hud);
+
+		var roll = document.getElementById('pitch');
+		var pitch = document.getElementById('pitch-container');
+		var height = pitch.getBoundingClientRect().height;
+		
+		this.state.simulation.orientation.getStream().changes().onValue((value) => {
+			var euler = value.toEulerAngles();
+			var p = euler.x*(-height/4);
+			pitch.style.transform = 'translate3d(0,' + p + 'px,0)';
+
+			var r = euler.z*90;
+			roll.style.transform = 'rotate3d(0,0,1,' + r + 'deg)';
+		});
 	}
 }
